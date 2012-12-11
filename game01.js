0a9f7ff7eefb4687f2dffe1dc807fc0df3a59aae
kivatek <shinya.hrsk@gmail.com>
2012-12-11 14:21:46 +0900

    戦車の画像の向きを変える処理をシンプルになるよう修正。

--- game01.js
+++ game01.js
@@ -35,6 +35,9 @@ var Tank = Class.create(Sprite, {
 		} else {
 			this.vx = this.vy = 0;
 
+			// 入力チェック前の向きを覚えておく。
+			var prevDirection = this.direction;
+
 			// 向き 0:下、1:左、2:右、3:上
 			if (game.input.left) {
 				this.direction = 1;
@@ -62,17 +65,19 @@ var Tank = Class.create(Sprite, {
 			} else if (game.input.b) {
 			}
 
+			if (prevDirection != this.direction) {
+				// 向きが変わる場合は使用する絵の番号を更新。
+				// 実はJsTank06までは見た目の向きは変わっていなかったが、変数の値は変更になっていた。
+				// そのため、戦車の見た目の向きとは違う方向に弾が撃ち出されていた。
+				this.frame = this.direction * 6 + this.pattern;
+			}
+
 			if (this.vx) {
 				var x = this.x + (this.vx ? this.vx / Math.abs(this.vx) * 32 : 0);
 				var y = this.y + (this.vy ? this.vy / Math.abs(this.vy) * 32 : 0);
 				if (0 <= x && x < SCREEN_WIDTH && !background.hitTest(x, y)) {
 					this.isMoving = true;
 					arguments.callee.call(this);
-				} else {
-					// 移動できない場合はその場で向きを変える。
-					// 実はJsTank06までは見た目の向きは変わっていなかったが、変数の値は変更になっていた。
-					// そのため、戦車の見た目の向きとは違う方向に弾が撃ち出されていた。
-					this.frame = this.direction * 6 + this.pattern;
 				}
 			}
 			if (this.vy) {
@@ -81,11 +86,6 @@ var Tank = Class.create(Sprite, {
 				if (0 <=y && y < SCREEN_HEIGHT && !background.hitTest(x, y)) {
 					this.isMoving = true;
 					arguments.callee.call(this);
-				} else {
-					// 移動できない場合はその場で向きを変える。
-					// 実はJsTank06までは見た目の向きは変わっていなかったが、変数の値は変更になっていた。
-					// そのため、戦車の見た目の向きとは違う方向に弾が撃ち出されていた。
-					this.frame = this.direction * 6 + this.pattern;
 				}
 			}
 		}
